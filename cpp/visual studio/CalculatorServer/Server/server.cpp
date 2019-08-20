08c5d09f8bb139908e9325f882f924857e9113c9
josua <josuabloess@gmx.de>
2019-08-20 22:31:45 +0200

    changed norm for template matching to sq_diff norm on H and S channel of H

--- cpp/visual studio/CalculatorServer/Server/server.cpp
+++ cpp/visual studio/CalculatorServer/Server/server.cpp
@@ -8,7 +8,7 @@ using namespace http;
 using namespace web::http::experimental::listener;
 
 /* IMPORTANT!!: this should be updated to the current release tag*/
-const std::string server::VERSION_TAG("v1.0.3");
+const std::string server::VERSION_TAG("v1.1.0");
 
 server::server(utility::string_t url) : m_listener(url)
 {
